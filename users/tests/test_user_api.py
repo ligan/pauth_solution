c9b3f2419c1667550ce43649bc2480772804c91f
Gan Li <baracouta@gmail.com>
2020-04-20 06:59:31 -0500

    Initial Commit

---
+++ users/tests/test_user_api.py
@@ -0,0 +1,22 @@
+from django.contrib.auth import get_user_model
+from rest_framework import status
+from rest_framework.reverse import reverse
+from rest_framework.test import APITestCase
+
+PASSWORD = 'pAssw0rd!'
+
+
+class AuthenticationTest(APITestCase):
+    def test_user_can_sign_up(self):
+        resposne = self.client.post(reverse('sign_up'),
+                                    data={
+                                        'email': 'user@example.com',
+                                        'password': PASSWORD,
+                                        'password_confirm': PASSWORD
+                                    })
+        user = get_user_model().objects.last()
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(response.data['id'], user.id)
+        self.assertEqual(response.data['username'], user.username)
+        self.assertEqual(response.data['first_name'], user.first_name)
+        self.assertEqual(response.data['last_name'], user.last_name)
